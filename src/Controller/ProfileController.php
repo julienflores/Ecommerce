22691c38950bf48b37595c3e712e6ca1b26eebff
Julien Flores <flores.julien@hotmail.fr>
2022-05-23 14:01:20 +0200

    Controllers

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -6,13 +6,22 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+
+#[Route('/profil', name: 'profile_')]
 class ProfileController extends AbstractController
 {
-    #[Route('/profil', name: 'profile')]
+    #[Route('/', name: 'index')]
     public function index(): Response
     {
         return $this->render('profile/index.html.twig', [
-            'controller_name' => 'ProfileController',
+            'controller_name' => 'Profil de l\'utilisateur',
+        ]);
+    }
+    #[Route('/commandes', name: 'orders')]
+    public function orders(): Response
+    {
+        return $this->render('profile/index.html.twig', [
+            'controller_name' => 'Commandes de l\'utilisateur',
         ]);
     }
 }
